33a1344fa09990afb1d1e0d5936a8ef0bbe96c49
Rusty <132937860+rustam432184798213651@users.noreply.github.com>
2024-03-09 16:32:25 +0000

    Add buggy map

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import "./styles.css";
+import * as L from "leaflet";
 
-document.getElementById("app").innerHTML = `
-Hello world`;
+const map = L.map("app").setView([51.505, -0.09], 13);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
