94adf9441e29d367d02651a6dd2601a032d4661f
Rusty <132937860+rustam432184798213651@users.noreply.github.com>
2024-03-09 17:20:15 +0000

    Add marker cluster

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "./styles.css";
 import * as L from "leaflet";
-
+import "leaflet.markercluster";
 const map = L.map("app").setView([51.505, -0.09], 13);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
@@ -8,11 +8,24 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-let marker = L.marker([51.5, -0.08]).addTo(map);
 const customUrl = L.icon({
-  iconUrl:
-    "https://images.hdqwalls.com/download/sunset-tree-red-ocean-sky-7w-2880x1800.jpg",
+  iconUrl: "https://www.flaticon.com/free-icon/placeholder_15112021.svg",
   iconSize: [30, 30],
 });
 
-var marker = L.marker([51.5, -0.08], { icon: customUrl }).addTo(map);
+// window.L instead of L ???
+const myClusterLayer = window.L.markerClusterGroup({
+  iconCreateFunction: function (cluster) {
+    return L.divIcon({
+      html: '<div class="cluster-div">' + cluster.getChildCount() + "</div>",
+    });
+  },
+});
+var marker = L.marker([51.5, -0.08]);
+var marker2 = L.marker([51.51, -0.09]);
+var marker3 = L.marker([51.5, -0.01]);
+
+myClusterLayer.addLayer(marker);
+myClusterLayer.addLayer(marker2);
+myClusterLayer.addLayer(marker3);
+map.addLayer(myClusterLayer);
