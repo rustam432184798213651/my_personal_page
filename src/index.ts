161ef90df430a5a08c674a25d155994a6be73390
Rusty <132937860+rustam432184798213651@users.noreply.github.com>
2024-03-09 16:47:23 +0000

    Add marker

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,12 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+
+let marker = L.marker([51.5, -0.08]).addTo(map);
+const customUrl = L.icon({
+  iconUrl:
+    "https://images.hdqwalls.com/download/sunset-tree-red-ocean-sky-7w-2880x1800.jpg",
+  iconSize: [30, 30],
+});
+
+var marker = L.marker([51.5, -0.08], { icon: customUrl }).addTo(map);
