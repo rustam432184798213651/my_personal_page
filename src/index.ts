53684ceb819fa4d4f039c4d02ca6867d2ea2e147
Rusty <132937860+rustam432184798213651@users.noreply.github.com>
2024-03-16 13:37:37 +0000

    Finished map

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "./styles.css";
 import * as L from "leaflet";
 import "leaflet.markercluster";
 import "leaflet-providers";
-const map = L.map("app").setView([51.505, -0.09], 13);
+const map = L.map("app").setView([55.803186, 37.41012], 13);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution:
@@ -23,14 +23,8 @@ const myClusterLayer = window.L.markerClusterGroup({
   },
 });
 // add markers
-var marker = L.marker([51.5, -0.08]);
-marker.bindPopup("<h3>Rusty created me. I am so glad to be alive! </h3>");
-var marker2 = L.marker([51.51, -0.09]);
-var marker3 = L.marker([51.5, -0.01]);
-
+var marker = L.marker([55.803186, 37.41012]);
+marker.bindPopup("<h3>MIEM</h3>");
 L.tileLayer.provider("CartoDB.Positron").addTo(map);
-
 myClusterLayer.addLayer(marker);
-myClusterLayer.addLayer(marker2);
-myClusterLayer.addLayer(marker3);
 map.addLayer(myClusterLayer);
